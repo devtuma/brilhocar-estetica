8deac56d3c630d645bd86987ba7111b750740666
Vicente Tuma <devtuma@gmail.com>
2026-08-06 00:49:26 -0300

    Novo banco de dados

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,24 +4,22 @@
 
    INSTRUÇÕES:
    1. Acesse console.firebase.google.com
-   2. Crie um projeto "brilhocar-estetica"
-   3. Habilite: Firestore Database, Authentication, Storage
-   4. Vá em Configurações do Projeto > Seus apps > Web
-   5. Copie a configuração e cole abaixo
+   2. Vá em Configurações do Projeto > Seus apps > Web
+   3. Copie a configuração e cole abaixo
    ============================================================ */
 
 var FIREBASE_CONFIG = window.FIREBASE_CONFIG = {
     /* --- FIREBASE (Brilho Car Estética Automotiva) --- */
-    apiKey:            'AIzaSyO8FaR87F73AkRLMFXJxFVJg4XFQJKVPQM',
-    authDomain:        'brilhocar-estetica.firebaseapp.com',
-    projectId:         'brilhocar-estetica',
-    storageBucket:     'brilhocar-estetica.firebasestorage.app',
-    messagingSenderId: '542562856492',
-    appId:             '1:542562856492:web:cafb728b8bb1d086c2eb89',
+    apiKey:            'AIzaSyB5CjK3f7NaXpvqabUHOO0IKiKqhWd6eIQ',
+    authDomain:        'brilhocar-estetica-9f14b.firebaseapp.com',
+    projectId:         'brilhocar-estetica-9f14b',
+    storageBucket:     'brilhocar-estetica-9f14b.firebasestorage.app',
+    messagingSenderId: '1084811695946',
+    appId:             '1:1084811695946:web:af8d9f7d4df175a03353b3',
 
     /* --- CREDENCIAIS ADMIN (Firebase Auth) --- */
-    ADMIN_EMAIL: 'admin@brilhocar.com',
-    ADMIN_PASS: 'BrilhoCar2025!',
+    ADMIN_EMAIL: 'devtuma@gmail.com',
+    ADMIN_PASS: '12345678',
 
     /* --- VERSÃO --- */
     VERSION: '2.0.0-firebase',
